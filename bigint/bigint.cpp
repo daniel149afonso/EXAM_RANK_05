39280e9a4ff2cf76e2749ba9f7d81aa168843447
Daniel Afonso <danielafonso@air-de-daniel.home>
2026-01-18 20:27:34 +0100

    starting addition bigint

--- bigint/bigint.cpp
+++ bigint/bigint.cpp
@@ -6,7 +6,49 @@
 /*   By: danielafonso <danielafonso@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 14:48:31 by danielafons       #+#    #+#             */
-/*   Updated: 2026/01/15 14:48:32 by danielafons      ###   ########.fr       */
+/*   Updated: 2026/01/18 20:26:24 by danielafons      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "bigint.hpp"
+
+int main ()
+{
+    std::vector<int> v1 = {1, 2, 3};
+    std::vector<int> v2 = {8, 9};
+    std::vector<int> v3;
+
+    while ()
+    {
+        /* code */
+    }
+    
+    for (int i = v1.size() - 1; i >= 0; i--)
+    {
+        for (int i = v2.size() - 1; i >= 0; i--)
+        {
+            
+        }
+    }
+    
+    int sum = v1[v1.size() - 1] + v2[v2.size() - 1];
+    int result = sum % 10;
+    int carry = sum / 10;
+    
+    v3.push_back(result);
+    
+    sum = v1[v1.size() - 2] + v2[v2.size() - 2] + carry;
+    result = sum % 10;
+    carry = sum / 10;
+
+    v3.push_back(result);
+    
+    sum = v1[v1.size() - 2] + 0 + carry;
+    v3.push_back(result);
+    
+    for (int i = 0; i < v3.size(); i++)
+    {
+        std::cout << v3[i] << std::endl;
+    }
+    return (0);
+}
--- bigint/main.cpp
+++ bigint/main.cpp
@@ -6,22 +6,43 @@
 /*   By: danielafonso <danielafonso@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 14:48:37 by danielafons       #+#    #+#             */
-/*   Updated: 2026/01/16 10:54:14 by danielafons      ###   ########.fr       */
+/*   Updated: 2026/01/18 16:21:21 by danielafons      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bigint.hpp"
+int ft_isdigit(std::string bigint)
+{
+    for (int i = 0; i < bigint.size(); i++)
+    {
+        if (!isdigit(bigint[i]))
+            return (0);
+    }
+    return (1);
+}
 
 int main(int argc, char **argv)
 {
     if (argc != 2)
         return (std::cerr << "Error: missing argument\n", 1);
+   
     int nb;
     std::string bigint;
     std::vector<int> v1;
+    
     bigint = argv[1];
-    nb = static_cast<int>(bigint[0]);
-    v1[0] = nb;
-    std::cout << v1[0] << std::endl;
+    if (!ft_isdigit(bigint))
+        return (std::cerr << "Error: only digits accepted\n", 1);
+    int i = 0;
+    while (bigint[i])
+    {
+        v1.push_back(bigint[i] - '0');
+        i++;
+    }
+    
+    for (int i = 0; i < v1.size(); i++)
+    {
+        std::cout << v1[i] << std::endl;
+    }
     return (0);
 }
