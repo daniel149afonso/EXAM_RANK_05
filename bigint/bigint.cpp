0ea6fe6368df872d4b1ebf5715147e67fa573ac2
Daniel Afonso <danielafonso@MacBook-Air-de-Daniel.local>
2026-01-19 15:27:10 +0100

    finished add option

--- bigint/bigint.cpp
+++ bigint/bigint.cpp
@@ -6,49 +6,54 @@
 /*   By: danielafonso <danielafonso@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 14:48:31 by danielafons       #+#    #+#             */
-/*   Updated: 2026/01/18 20:26:24 by danielafons      ###   ########.fr       */
+/*   Updated: 2026/01/19 12:30:45 by danielafons      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "bigint.hpp"
+#include "Bigint.hpp"
 
-int main ()
+Bigint::Bigint(const std::string& str): str(str)
 {
-    std::vector<int> v1 = {1, 2, 3};
-    std::vector<int> v2 = {8, 9};
+    
+}
+
+Bigint& Bigint::operator+(const Bigint& other)
+{
+    
+    std::vector<int> this.v1 = {0, 0, 0, 0, 0, 0};
+    std::vector<int> other.v2 = {0, 0};
     std::vector<int> v3;
 
-    while ()
-    {
-        /* code */
-    }
-    
-    for (int i = v1.size() - 1; i >= 0; i--)
-    {
-        for (int i = v2.size() - 1; i >= 0; i--)
-        {
-            
-        }
-    }
-    
-    int sum = v1[v1.size() - 1] + v2[v2.size() - 1];
-    int result = sum % 10;
-    int carry = sum / 10;
-    
-    v3.push_back(result);
-    
-    sum = v1[v1.size() - 2] + v2[v2.size() - 2] + carry;
-    result = sum % 10;
-    carry = sum / 10;
+    if (this.v1.size() > 1 || other.v2.size() > 1)
+        return (1);
 
-    v3.push_back(result);
-    
-    sum = v1[v1.size() - 2] + 0 + carry;
-    v3.push_back(result);
-    
-    for (int i = 0; i < v3.size(); i++)
+    int i = static_cast<int>(this.v1.size() - 1);
+    int j = static_cast<int>(other.v2.size() - 1);
+    int sum, result, carry = 0;
+    while (i >= 0 || j >= 0)
     {
-        std::cout << v3[i] << std::endl;
+        int a = (i >= 0) ? this.v1[i] : 0;
+        int b = (j >= 0) ? other.v2[j] : 0;
+
+        sum = a + b + carry;
+        result = sum % 10;
+        carry = sum / 10;
+
+        v3.push_back(result);
+        i--;
+        j--;
     }
+    if (carry)
+        v3.push_back(carry);
+    i = static_cast<int>(v3.size() - 1);
+    while (v3.size() > 1 && v3.back() == 0)
+    {
+        v3.pop_back();
+        i--;
+    }
+
+    for (int i = static_cast<int>(v3.size() - 1); i > -1; i--)
+        std::cout << v3[i];
+    std::cout << std::endl;
     return (0);
 }
--- bigint/bigint.hpp
+++ bigint/bigint.hpp
@@ -6,7 +6,7 @@
 /*   By: danielafonso <danielafonso@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 14:48:27 by danielafons       #+#    #+#             */
-/*   Updated: 2026/01/15 17:53:57 by danielafons      ###   ########.fr       */
+/*   Updated: 2026/01/19 12:26:52 by danielafons      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,15 @@
 #include <iostream>
 #include <vector>
 
-class bigint {
+class Bigint {
     private:
         unsigned int bigint;
+        std::vector<int> v1;
+        std::string str;
     public:
-    
-        
+    Bigint(const std::string& str);
+    Bigint& operator+(const Bigint& other);
+    Bigint& operator<<(const Bigint& other);
 };
 
 #endif
